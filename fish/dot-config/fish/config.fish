01c58b04823420cf849405a7fd7284e03eefb7f2
Samuel P. Jones <sjones@flexport.com>
2025-09-11 13:19:41 -0700

    added fish coniguration for work

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,3 +1,16 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    switch (uname)
+        case Darwin
+            fish_add_path /opt/homebrew/bin
+    end
+end
+
+if status --is-login
+    # login shell stuff here
+end
+
+if test -d $HOME/flexport
+    echo "Configuring for work"
+    set dir (dirname (status -f))
+    source $dir/work_config.fish
 end
---
+++ fish/dot-config/fish/work_config.fish
@@ -0,0 +1,51 @@
+if status is-interactive
+    # exported for all fish shells (and write to disk)
+    set -Ux JAVA_HOME /Library/Java/JavaVirtualMachines/amazon-corretto-21.jdk/Contents/Home
+    set -Ux GITHUB_USERNAME sjones
+    set -Ux SOURCE_SCRIPT_DIR /Users/sjones/IdeaProjects/infra-deployer
+    set -Ux SNOWFLAKE_USERNAME "SJONES@FLEXPORT.COM"
+    set -Ux KMO_HOME /Users/sjones/Documents/code/kimono/
+    set -Ux KMO_SNOWFLAKE_USERNAME "SJONES@FLEXPORT.COM"
+    set -Ux AWS_REGION us-east-1
+    set -Ux SNOWFLAKE_WAREHOUSE_XS "DBT_DEV_XS"
+    set -Ux SNOWFLAKE_WAREHOUSE_S "DBT_DEV_S"
+    set -Ux SNOWFLAKE_WAREHOUSE_M "DBT_DEV_M"
+    set -Ux SNOWFLAKE_WAREHOUSE_L "DBT_DEV_L"
+    set -Ux SNOWFLAKE_WAREHOUSE_XL "DBT_DEV_XL"
+    set -Ux SNOWFLAKE_WAREHOUSE_DYNAMIC_M "DBT_DEV_M"
+    set -Ux DBT_PROJECT_DIR "/Users/sjones/Documents/code/kimono/transform/dbt_transforms"
+    set -Ux DBT_PROFILES_DIR "/Users/sjones/Documents/code/kimono/transform/dbt_transforms"
+    set -Ux PYENV_ROOT $HOME/.pyenv
+  
+    alias k="kubectl"
+    alias mpr="/Users/sjones/flexport/mpr"
+    alias bastion_ssh='~/flexport/env-improvement/bin/bastion ssh'
+    alias dbt-defer="$DBT_PROJECT_DIR/scripts/dbt-defer.sh"
+    alias my_kimono='echo /Users/sjones/Documents/code/kimono/transform'
+
+    fish_add_path /opt/homebrew/Cellar/llvm@14/14.0.6/bin
+    fish_add_path $(go env GOPATH)/bin
+    if test -d $PYENV_ROOT/bin 
+        fish_add_path "$PYENV_ROOT/bin"
+    end
+    fish_add_path "/Users/sjones/.local/bin"
+    if command -q pyenv
+    else 
+        echo "Couldn't find pyenv command, adding pyenv to path"
+        fish_add_path "$PYENV_ROOT/bin"
+    end
+    pyenv init - fish | source
+    # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+    fish_add_path "$HOME/.rvm/bin"
+end
+
+if status --is-login
+    set -Ux NVM_DIR "$HOME/.nvm"
+    set NVM_INSTALL_DIR "$HOMEBREW_PREFIX/opt/nvm"
+    if test -s "$NVM_INSTALL_DIR/nvm.sh" 
+        source "$NVM_INSTALL_DIR/nvm.sh"
+    end
+    if test -s "$NVM_INSTALL_DIR/etc/bash_completion.d/nvm"
+        source "$NVM_INSTALL_DIR/etc/bash_completion.d/nvm"
+    end
+end
