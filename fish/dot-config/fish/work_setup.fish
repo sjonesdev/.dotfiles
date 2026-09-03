7bc7e72bab805298ca0839aaab5518cbafc5f0eb
Samuel P. Jones <sjones@flexport.com>
2026-09-03 16:11:22 -0700

    update dotfiles, improve nvm handling, add opencode

---
+++ fish/dot-config/fish/work_setup.fish
@@ -0,0 +1,5 @@
+# TODO add naws setup
+nvm use
+npm config set @ffa:registry https://artifactory.flexport.io/artifactory/api/npm/ffa-infra-npm-dev-local
+npm install -g @ffa/naws-cli
+git lfs install
