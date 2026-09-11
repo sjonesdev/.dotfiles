41c75e201b3dc82bd0e531cffb0d3710ab367be4
Samuel P. Jones <sjones@flexport.com>
2026-09-11 11:03:47 -0700

    add chatgpt and codex to work packages, add some print statements

--- setup/work_setup.fish
+++ setup/work_setup.fish
@@ -1,5 +1,8 @@
 # TODO add naws setup
+echo "Work setup"
+echo "Setting up global JS toolchain"
 nvm use
 npm config set @ffa:registry https://artifactory.flexport.io/artifactory/api/npm/ffa-infra-npm-dev-local
 npm install -g @ffa/naws-cli
+echo 
 git lfs install
